bec319a15f502da28710d5d573036d5f282da7fe
Noe <8528445+NoeRGuerra@users.noreply.github.com>
2024-07-09 22:30:15 -0400

    Add demo to-do lists and display logic in index.js
    - Implement createDemoLists function to create sample to-do lists
    - Add populateLists function to dynamically create list buttons
    - Implement clearLists and clearToDoList helper functions
    - Add displayToDoList function to render tasks with checkboxes
    - Add basic styling in style.css

---
+++ src/index.js
@@ -0,0 +1,109 @@
+import './views/style.css';
+const ToDoList = require('./models/ToDoList');
+const Task = require('./models/Task');
+const Step = require('./models/Step');
+
+const existingLists = [];
+
+function createDemoLists() {
+    const cleanKitchenList = new ToDoList('Clean Kitchen');
+
+    // Create tasks for the "Clean Kitchen" list
+    const task1 = new Task('Wash dishes');
+    task1.addStep(new Step('Gather all dishes'));
+
+    const task2 = new Task('Sweep floor');
+    task2.addStep(new Step('Sweep entire kitchen floor'));
+
+    const task3 = new Task('Mop the kitchen');
+    task3.addStep(new Step('Mop with soapy water'));
+
+    cleanKitchenList.addTask(task1);
+    cleanKitchenList.addTask(task2);
+    cleanKitchenList.addTask(task3);
+
+    // Create a new to-do list for "Clean Room"
+    const cleanRoomList = new ToDoList('Clean Room');
+
+    // Create tasks for the "Clean Room" list
+    const task4 = new Task('Organize desk');
+    const task5 = new Task('Vacuum carpet');
+    task5.addStep(new Step('Vacuum the entire carpet'));
+
+    cleanRoomList.addTask(task4);
+    cleanRoomList.addTask(task5);
+
+    // Create a new to-do list for "Get groceries"
+    const getGroceriesList = new ToDoList('Get Groceries');
+
+    // Create tasks for the "Get Groceries" list
+    const task6 = new Task('Buy milk');
+    const task7 = new Task('Buy eggs');
+    const task8 = new Task('Buy bread');
+    const task9 = new Task('Buy vegetables');
+    const task10 = new Task('Buy fruit');
+
+    getGroceriesList.addTask(task6);
+    getGroceriesList.addTask(task7);
+    getGroceriesList.addTask(task8);
+    getGroceriesList.addTask(task9);
+    getGroceriesList.addTask(task10);
+
+    existingLists.push(cleanKitchenList);
+    existingLists.push(cleanRoomList);
+    existingLists.push(getGroceriesList);
+}
+
+function populateLists() {
+    const listsContainer = document.querySelector("#lists");
+    for (let list of existingLists) {
+        const listParagraph = document.createElement('button');
+        listParagraph.textContent = list.name;
+        listsContainer.appendChild(listParagraph);
+        listParagraph.addEventListener('click', () => {
+            clearToDoList();
+            displayToDoList(list);
+        });
+    }
+}
+
+function clearLists() {
+    const listsContainer = document.querySelector("#lists");
+    listsContainer.replaceChildren();
+}
+
+function clearToDoList() {
+    const rightDiv = document.querySelector(".right");
+    rightDiv.replaceChildren();
+}
+
+function displayToDoList(ToDoList) {
+    console.log(ToDoList);
+    const rightDiv = document.querySelector(".right");
+    const header = document.createElement('h2');
+    header.textContent = ToDoList.name;
+    rightDiv.appendChild(header);
+
+    for (let i = 0; i < ToDoList.listOfTasks.length; i++ ) {
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = i;
+        checkbox.name = i;
+        checkbox.value = i;
+        const label = document.createElement('label');
+        label.htmlFor = i;
+        label.textContent = ToDoList.listOfTasks[i].name;
+        const br = document.createElement('br');
+        rightDiv.appendChild(checkbox);
+        rightDiv.appendChild(label);
+        rightDiv.appendChild(br);
+    }
+}
+
+createDemoLists();
+for (let list of existingLists) {
+    console.log(list.name);
+}
+
+populateLists();
+displayToDoList(existingLists[0]);
